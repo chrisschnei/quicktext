48ad24eda02d613c1236b3261a6a062f665af627
Christian Schneider <christian.schneider3@gmx.net>
2020-02-19 16:29:28 +0100

    First issue 57 implementation

--- chrome/content/main.js
+++ chrome/content/main.js
@@ -6,7 +6,7 @@ var quicktext = {
   {
     window.close();
   }
-,  
+,
   onunloadoptions: function ()
   {
     this.openSettings();
@@ -19,6 +19,7 @@ var quicktext = {
   }
 }
 
+/////// NEW ///////////////////////////
 // For some reason I cannot add an onload to the overlay "main.xul", so I have
 // to call this manually, but only if it is called from messenger.xul
 if (window.location.href == "chrome://messenger/content/messenger.xul") {
@@ -30,3 +31,30 @@ async function main() {
   gQuicktext.loadLocales(document);
   await gQuicktext.loadSettings(false);
 }
+
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+
+  if (keyName === 'Control') {
+    // do not alert when only Control key is pressed.
+    return;
+  }
+
+  if (event.ctrlKey) {
+    // Even though event.key is not 'Control' (e.g., 'a' is pressed),
+    // event.ctrlKey may be true if Ctrl key is pressed at the same time.
+    alert(`Combination of ctrlKey + ${keyName}`);
+  } else {
+    alert(`Key pressed ${keyName}`);
+  }
+}, false);
+
+document.addEventListener('keyup', (event) => {
+  const keyName = event.key;
+
+  // As the user releases the Ctrl key, the key is no longer active,
+  // so event.ctrlKey is false.
+  if (keyName === 'Control') {
+    alert('Control key was released');
+  }
+}, false);
